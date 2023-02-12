dc48a37a5293ced18157761e0d9738b830880872
rMaloberti <r.maloberti@yahoo.com>
2023-02-12 20:21:11 +0100

    [navlinks] create component and add style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import '../style/components/Navbar.css';
 import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 const Navbar = ({ location }) => {
   return (
     <div className={`navbar navbar-${location}`}>
-      {location === 'top' ? <Logo /> : <p className="navbar__copyright">Copiryght, SportSee 2020</p>}
-      {location === 'top' ? <p>link-list</p> : <p>icon-list</p>}
+      {location === 'top' ? (
+        <Logo />
+      ) : (
+        <p className="navbar__copyright">Copiryght, SportSee 2020</p>
+      )}
+      {location === 'top' ? <NavLinks /> : <p>icon-list</p>}
     </div>
   );
 };
