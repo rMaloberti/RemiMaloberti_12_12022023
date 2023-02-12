15c1691252d5100bcbb1195fd053ddb29a1fe86f
rMaloberti <r.maloberti@yahoo.com>
2023-02-12 20:09:23 +0100

    [nav] add style to the left navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Logo from './Logo';
 const Navbar = ({ location }) => {
   return (
     <div className={`navbar navbar-${location}`}>
-      {location === 'top' ? <Logo /> : <p>Copyright</p>}
+      {location === 'top' ? <Logo /> : <p className="navbar__copyright">Copiryght, SportSee 2020</p>}
       {location === 'top' ? <p>link-list</p> : <p>icon-list</p>}
     </div>
   );
