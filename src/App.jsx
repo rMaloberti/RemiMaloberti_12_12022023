f6224c01fbcee8fbece095ea81a545f25bf0de53
rMaloberti <r.maloberti@yahoo.com>
2023-02-12 17:58:20 +0100

    [nav] create navbar and logo components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import Navbar from './components/Navbar';
 import './style/App.css';
 
 const App = () => {
-  return;
+  return (
+    <div className="app">
+      <div className="nav">
+        <Navbar location="top" />
+        <Navbar location="left" />
+      </div>
+      <div className="main app__main"></div>
+    </div>
+  );
 };
 
 export default App;
